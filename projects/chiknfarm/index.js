6ab1efcf00115e1b1709d57644d96e3e085bb51b
Chikn Farm <chiknfarm10000@gmail.com>
2022-01-06 00:40:21 +1100

    use the sdk

--- projects/chiknfarm/index.js
+++ projects/chiknfarm/index.js
@@ -1,21 +1,30 @@
-const retry = require('async-retry')
-const axios = require("axios");
+const sdk = require("@defillama/sdk");
+const abi = require("./abi.json");
 const BigNumber = require("bignumber.js");
 
-async function fetch() {
-    let price_feed = await retry(async bail => await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=chikn-egg&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true'))
-    let response = await retry(async bail => await axios.get('https://cdn-b.chikn.farm/api/feed/staked'))
-    let tvl = new BigNumber(response.data.totalStakedAmount).toFixed(2);
-    return (tvl * price_feed.data['chikn-egg'].usd);
-}
+const EggContract = "0x7761E2338B35bCEB6BdA6ce477EF012bde7aE611";
+const FeedContract = "0xab592d197ACc575D16C3346f4EB70C703F308D1E";
 
-async function tvl() {
-    return 0
-}
+
+const avaxTvl = async (_, _ethBlock, chainBlocks) => {
+    const stakedEggs = (
+        await sdk.api.abi.call({
+            abi: abi.TotalEggStaked,
+            target: FeedContract,
+            chain: "avax",
+            block: chainBlocks["avax"],
+        })
+    ).output;
+    let stakedAmt = new BigNumber(stakedEggs).toFixed(0);
+    return {
+        [EggContract]: stakedAmt
+    };
+};
 
 module.exports = {
-    staking: {
-        fetch: fetch
+    avalanche: {
+        methodology: "Tokens staked to gain Feed is counted as TVL",
+        tvl: avaxTvl
     },
-    fetch: tvl
+    tvl: avaxTvl
 }
